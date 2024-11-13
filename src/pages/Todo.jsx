2f1d0622c25b66742c3ace106f99c64cfdc22a4e
ashish <148787104+ashish3410@users.noreply.github.com>
2024-11-13 23:01:42 +0530

    Update Todo.jsx

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -4,7 +4,7 @@ function Home() {
     const navigate = useNavigate()
     return (
         <>
-            <section style={{ backgroundImage: `url('src/assets/common-bg.svg')` }} className=' flex flex-col justify-center text-center h-full pt-48 z-10'>
+            <section style={{ backgroundImage: `url('https://www.rammaheshwari.com/assets/svg/common-bg.svg')` }} className=' flex flex-col justify-center text-center h-full pt-48 z-10'>
                 <div>
                     <h1 className='text-5xl font-bold p-4'>TODO USING REACT TOOLKIT</h1>
                 </div>
